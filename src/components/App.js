f1794628fb46fa24e871fb3083a08ad9da1cfef8
kishore <4nm15is025@nmamit.in>
2018-12-29 18:02:01 +0530

    storing the fetched images in state

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import SearchBar from './SearchBar';
 import axios from 'axios';
 
 class App extends React.Component{
-    async onSearchSubmit(term){
+    state={ images:[] };
+     onSearchSubmit = async (term)=>{
         //console.log(term);
         let response = await axios.get('https://api.unsplash.com/search/photos',{
             params:{
@@ -13,18 +14,20 @@ class App extends React.Component{
             Authorization: "Client-ID 70163fbb4c0f77e17e2d4e26a013d5f73c01a1598a61dbc3d4e9908b5268baba"
            } 
         });
-        console.log(response.data.results)
+        console.log(response.data.results);
+        this.setState({images:response.data.results});
         /*
-        adding axios and making get request to the unsplash api to get the list 
-        of image url's on search term
+        onSearchTerm is made as a arrow function to bind the 
+        "this" keyword while calling the setstate function
         */
     }
     render(){
         return(
             <div className="ui container" style={{marginTop:"10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Images Fetched:{this.state.images.length} images
             </div>
-        )
+        );
     }
 }
 
