4ce4efe5cc45c5c1807ea897e274c58fc522b064
kishore <4nm15is025@nmamit.in>
2018-12-29 17:49:34 +0530

    adding axios and making get request to unspalsh api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends React.Component{
-    onSearchSubmit(term){
-        console.log(term);
+    async onSearchSubmit(term){
+        //console.log(term);
+        let response = await axios.get('https://api.unsplash.com/search/photos',{
+            params:{
+                query:term
+            },
+           headers:{
+            Authorization: "Client-ID 70163fbb4c0f77e17e2d4e26a013d5f73c01a1598a61dbc3d4e9908b5268baba"
+           } 
+        });
+        console.log(response.data.results)
+        /*
+        adding axios and making get request to the unsplash api to get the list 
+        of image url's on search term
+        */
     }
     render(){
         return(
