3ed35eca798e23adc56d91a512013cb85481194b
kishore <4nm15is025@nmamit.in>
2018-12-30 13:10:44 +0530

    setting the grid row end property from the computed spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,15 +3,24 @@ import React from'react';
 class ImageCard extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            spans:0
+        }
+
         this.imageRef = React.createRef();
     }
     componentDidMount(){
-        console.log(this.imageRef.current.clientHeight);
+        this.imageRef.current.addEventListener('load', this.setSpan);
+    }
+    setSpan =()=>{
+        let clent = this.imageRef.current.clientHeight;
+        let spans = Math.ceil(clent/10);
+        this.setState({spans});
     }
      render(){
         const { description, urls } = this.props.images;
         return (
-            <img ref={this.imageRef} alt={description} src={urls.regular} />
+            <img ref={this.imageRef} style={{ gridRowEnd:`span ${this.state.spans}` }} alt={description} src={urls.regular} />
         )
      }
 }
